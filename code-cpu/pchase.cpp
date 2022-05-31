c3665b4ccaf47f5a1cd0b6ed872a49b8e4017d94
Libre Liu <libreliu@foxmail.com>
2022-05-31 17:29:21 +0800

    pchase wip

--- code-cpu/pchase.cpp
+++ code-cpu/pchase.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
 #include <chrono>
@@ -8,6 +9,36 @@
 
 #define KiB 1024
 #define MiB (1024 * KiB)
+
+#ifdef __MSC_VER
+#define NOINLINE __declspec(noinline)
+
+#include <windows.h>
+#endif
+
+
+template<int _npad>
+struct Node {
+    Node* next;
+    unsigned int npad[_npad];
+};
+
+size_t get_page_size() {
+    static size_t pageSize = 0;
+
+    if (pageSize != 0) {
+        return pageSize;
+    }
+
+#ifdef __MSC_VER
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    pageSize = si.dwPageSize;
+#endif
+
+    printf("The page size for this system is %u bytes.\n", pageSize);
+    return pageSize;
+}
 
 // return time measured in sec
 template<auto _Func, typename ..._Args>
@@ -22,27 +53,40 @@ double timeit(_Args&&... args) {
     return diff.count();
 }
 
-__declspec(noinline)
-void do_chase(void* &res, void* start, size_t numLoop) {
-    void* p = start;
+template<typename NodeT, size_t numLoop>
+NOINLINE
+void do_chase(NodeT *start, NodeT *&res) {
+    NodeT* p = start;
     for (size_t i = 0; i < numLoop; i++) {
-        p = *(void**)(p);
+        p = start->next;
     }
     res = p;
 }
 
-template<typename _Obj_t>
-_Obj_t* prepare_full_random(_Obj_t *&arr, size_t size) {
+// workingSetSize: 
+template<typename NodeT>
+void prepare_full_random(NodeT *&arr, size_t workingSetSize) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> distrib(0, size - 1);
 
-    arr = (_Obj_t*)malloc(size * sizeof(_Obj_t));
-    for (size_t i = 0; i < size; i++) {
-        arr[i] = distrib(gen);
+    size_t numElements = workingSetSize / sizeof(NodeT);
+    arr = (NodeT*)std::aligned_alloc(get_page_size(), workingSetSize);
+
+    // shuffle [0, ..., numElements - 1]
+    std::vector<size_t> vec(numElements, 0);
+    for (size_t i = 0; i < numElements; i++) {
+        vec[i] = i + (size_t)arr;
     }
 
-    return arr;
+    std::shuffle(vec.begin(), vec.end(), gen);
+    for (size_t i = 0; i < numElements; i++) {
+        arr[i].next = (NodeT*)vec[i];
+    }
+}
+
+template<typename NodeT>
+void verify_node_list(NodeT *arr, size_t workingSetSize) {
+    size_t expectedElements = workingSetSize / sizeof(NodeT);
 }
 
 
